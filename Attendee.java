c5b1908255188facf21239a84443274002a99141
Bob283527 <jiangjon27@7hills.org>
2025-12-01 21:16:31 -0500

    fixed some things

--- Attendee.java
+++ Attendee.java
@@ -1,29 +1,36 @@
 public class Attendee {
-	
-	private nameLast;
-	private nameFirst;
-	private companyNum;
-	private listNum;
-	
-	public Attendee(String numLast, String numFirst, int companyNum, int listNum) {
-		numLast = nL;
-		numFirst = nF;
-		companyNum = cN;
-		listNum = lN;
-	}  
-	
-	public String getnameFirst() {
-		return nameFirst;
-	}
-	public String getnameLast() {
-		return nameLast;
-	}
-	public int getlistNum() {
-		return listNum;
-	}
-	public int getcompanyNum() {
-		return companyNum;
-	}
-	
-}
+	//to hold info for each person
 
+	//priv vars
+    private String nameLast;
+    private String nameFirst;
+    
+    
+    private int companyNum;
+    private int listNum;
+
+//constructor for info from file or the input and load into thh object
+    public Attendee(String nL, String nF, int cN, int lN) {
+        nameLast = nL;
+        nameFirst = nF;
+        companyNum = cN;
+        listNum = lN;
+    }
+//getters for info later
+    public String getnameFirst() {
+        return nameFirst;
+    }
+
+    public String getnameLast() {
+        return nameLast;
+    }
+
+    public int getcompanyNum() {
+        return companyNum;
+    }
+
+    public int getlistNum() {
+        return listNum;
+    }
+
+}
