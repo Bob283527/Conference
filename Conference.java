841c19f75da26ad653e08a21c4645ec6e447b0a0
Bob283527 <jiangjon27@7hills.org>
2025-12-01 21:27:28 -0500

    Organized and fixed some syntax and stuff and adding a 100 person limit

--- Conference.java
+++ Conference.java
@@ -4,59 +4,65 @@ import java.util.*;
 public class Conference {
 	//add scanner for registratin input later
 	Scanner registerScan = new Scanner(System.in);
-	//add private cars for later use
+	//add private cars for later use, keeping track of the comapny stuff and tables
 	private int numTables;
+	private int numCompanies;
+    private int companiesNumber;
+    private String companiesName;
+
+	//attendee has 150 bc may be more in file, only 100 in
+	private Attendee[] attendeeArray = new Attendee[150];
+
+	//company seat counter
+	//companySeats[c][t], chekc idf company c is alrdy at table t
+	private int[][] companySeats = new int[20][5];
 	//initialize the vars
 	public Conference(int nC, int nT, int cNum, String cName) {
-		numTables = nT;
-		numCompanies = nC;
-		companiesNumber = cNum;
-		companiesName = cName;
-	}  
-	    // Add any getters and toString methods that you need
-	
-	public int getnumCompanies() {
-		return numCompanies;
-	}
-	public int getnumTables() {
-		return numTables; 
-	}
-	public int getcompaniesNumber() {
-		return companiesNumber;
-	}
-	public String getcompaniesName() {
-		return companiesName;
-	}
+        numCompanies = nC;
+        numTables = nT;
+        companiesNumber = cNum;
+        companiesName = cName;
+    }
+	//getters for toString
+    public int getnumCompanies() {
+        return numCompanies;
+    }
 
-		
+    public int getnumTables() {
+        return numTables;
+    }
+
+    public int getcompaniesNumber() {
+        return companiesNumber;
+    }
+
+    public String getcompaniesName() {
+        return companiesName;
+    }
+
 	//scans the file and loads the data into the memory will have less attendees on the file than what i put in tge array
 	//because of registration later so I will have to insert the new people into the file
 	//then we can randomize who gets in as the limit is 100, so pre-registered will always be first to get in
 	//then randomly choose the other last few people who registered
 	//loads both files
-	private Attendee[] attendeeArray = new Attendee[150];
-	private Companies companiesArray[] = new Companies[20];
 
+	//loads data from file
 	private void loadData() throws IOException {
+		
         File file = new File("confGuests.txt");
-        File fileC = new File("companies.txt");
-        
         Scanner scan = new Scanner(file);
-        Scanner scanC = new Scanner(fileC);
         
         if (scan.hasNext()) {
             scan.nextLine();
         }
-         if (scanC.hasNext()) {
-            scanC.nextLine();
-        }
+
 	//loops through file and attendees and assigns which part of the array indicates what
 		int i = 0;
 		while (scan.hasNext() && i < attendeeArray.length) {
-			//splits the information into parts 
+			//reads entire line 
 			String line = scan.nextLine();
+			//comma split
 			String[] parts = line.split(",");
-			int[] parts2 = line.split(",");
 			
 			int lN = parts[0];
 			int cN = parts[3];
@@ -70,11 +76,19 @@ public class Conference {
 		//debugging purposes
 		scan.close();
 		System.out.println(i + "Attendees loaded");
+		
+		//checks for only 100 ppl
+		if(i > 100) {
+            System.out.println("Keeping only first 100.");
+		}
 	}
 	//Need to make a method or two to place them into their seats now
 	//since i have the attendees and their information I can probably randomize them and make sure the cN is not equal to eachother
 	//make 5 tables maybe?
 	//10 people per table make sure their cN isnt the same and that they arent the same person
+	
+	//ask user if they want to add if less than 100 ppl
+	
 	public seatPlacement() {
 	int i = 0;
 		while (scan.hasNext() && i < attendeeArray.length) {
